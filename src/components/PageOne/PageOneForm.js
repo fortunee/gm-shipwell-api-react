687306af1277fc12af8e72412fa5f33a4fd5aea3
Fortune Ekeruo <legalmody@gmail.com>
2018-07-30 23:40:54 +0100

    handle onsubmit of form

--- src/components/PageOne/PageOneForm.js
+++ src/components/PageOne/PageOneForm.js
@@ -9,19 +9,37 @@ class PageOneForm extends Component {
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
         const field = e.target.name;
         const value = e.target.value;
 
-        this.setState({ field: value });
+        this.setState({ [field]: value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const { to, from } = this.state;
+        const addresses = { to, from }
+        const URL = 'https://dev-api.shipwell.com/v2/locations/addresses/validate';
+
+        fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(addresses)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
     }
 
     render() {
         return (
             <div className="page-form">
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label>To: </label>
                         <input type="text" name="to" onChange={this.onChange} value={this.state.to}/>
