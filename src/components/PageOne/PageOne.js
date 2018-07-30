8f8bf56d1f4de303884766ac2693e8ee851763e1
Fortune Ekeruo <legalmody@gmail.com>
2018-07-30 14:25:21 +0100

    set data returned from shipwell to state and display first_name within render

--- src/components/PageOne/PageOne.js
+++ src/components/PageOne/PageOne.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 class PageOne extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {
+                user: {},
+                company: {}
+            }
+        }
+    }
     componentWillMount() {
         const URL = 'https://dev-api.shipwell.com/v2/auth/me';
         const token = 'Token 4c4547fe6ad68c57cbac0a8cfbfec35b';
@@ -9,14 +18,18 @@ class PageOne extends Component {
         }
         fetch(URL, { headers })
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => this.setState({ data }));
     }
 
     render() {
+        // const { user = {} } = this.state.data;
+        const { user } = this.state.data;
+
         return (
-        <div>
-            <h1>PageOne</h1> 
-        </div>
+            <div>
+                <h1>PageOne</h1>
+                <h2>First name: {user.first_name}</h2>
+            </div>
         )
     }
 }
